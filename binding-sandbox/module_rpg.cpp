ae19c0e42eecfcc419ade29271f8776ccb13ffad
刘皓 <whiteaxe@tuta.io>
2025-04-09 23:52:12 -0400

    Reconcile the standalone and libretro build systems

---
+++ binding-sandbox/module_rpg.cpp
@@ -0,0 +1,3 @@
+#include "../binding/module_rpg1.rb.xxd"
+#include "../binding/module_rpg2.rb.xxd"
+#include "../binding/module_rpg3.rb.xxd"
--- src/stb_image_malloc.h
+++ src/stb_image_malloc.h
@@ -1,10 +1,10 @@
 #ifndef MKXPZ_STB_IMAGE_MALLOC_H
 #define MKXPZ_STB_IMAGE_MALLOC_H
 
-#include <cstdlib>
+#include <stdlib.h>
 
-#define STBI_MALLOC std::malloc
-#define STBI_REALLOC std::realloc
-#define STBI_FREE std::free
+#define STBI_MALLOC malloc
+#define STBI_REALLOC realloc
+#define STBI_FREE free
 
 #endif // MKXPZ_STB_IMAGE_MALLOC_H
