09ec1db6f469ebef5c5c199f41b7fd8cc5c559fa
刘皓 <whiteaxe@tuta.io>
2025-05-19 15:16:34 -0400

    Fix race condition in `Movie` destructor

--- src/display/graphics.cpp
+++ src/display/graphics.cpp
@@ -397,6 +397,12 @@ struct Movie
     ~Movie()
     {
         if (hasAudio) {
+            audioThreadTermReq.set();
+            if(audioThread) {
+                SDL_WaitThread(audioThread, 0);
+                audioThread = 0;
+            }
+
             if (audioQueueTail) {
                 THEORAPLAY_freeAudio(audioQueueTail->audio);
             }
@@ -407,11 +413,6 @@ struct Movie
             }
             audioQueueHead = NULL;
             SDL_DestroyMutex(audioMutex);
-            audioThreadTermReq.set();
-            if(audioThread) {
-                SDL_WaitThread(audioThread, 0);
-                audioThread = 0;
-            }
             alSourceStop(audioSource);
             alDeleteSources(1, &audioSource);
             alDeleteBuffers(STREAM_BUFS, alBuffers);
