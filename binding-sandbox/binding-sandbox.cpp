accb5948eaac958e7422020cf6ac663386b1d2a7
刘皓 <whiteaxe@tuta.io>
2025-07-13 20:59:33 -0400

    Update libretro-common and add Irish language in binding-sandbox.cpp

--- binding-sandbox/binding-sandbox.cpp
+++ binding-sandbox/binding-sandbox.cpp
@@ -384,6 +384,7 @@ static VALUE user_language(VALUE self) {
         case RETRO_LANGUAGE_BELARUSIAN: str = "be_BY"; break;
         case RETRO_LANGUAGE_GALICIAN: str = "gl_ES"; break;
         case RETRO_LANGUAGE_NORWEGIAN: str = "no_NO"; break;
+        case RETRO_LANGUAGE_IRISH: str = "ga_IE"; break;
     }
 
     return sb()->bind<struct rb_str_new_cstr>()()(str);
