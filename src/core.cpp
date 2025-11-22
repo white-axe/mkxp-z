90eae3416285074c2810ad7fb0d7fad3f6dabd8c
刘皓 <whiteaxe@tuta.io>
2025-11-22 10:51:32 -0500

    Add OpenGL 3.3 to OpenGL version selection in src/core.cpp

--- src/core.cpp
+++ src/core.cpp
@@ -2457,6 +2457,8 @@ extern "C" RETRO_API bool retro_load_game(const struct retro_game_info *info) {
         LOG_PRINT(RETRO_LOG_INFO, "Using OpenGL 4.1 graphics driver\n");
     } else if (hw_render.context_type = RETRO_HW_CONTEXT_OPENGL_CORE, hw_render.version_major = 4, hw_render.version_minor = 0, environment(RETRO_ENVIRONMENT_SET_HW_RENDER, &hw_render)) {
         LOG_PRINT(RETRO_LOG_INFO, "Using OpenGL 4.0 graphics driver\n");
+    } else if (hw_render.context_type = RETRO_HW_CONTEXT_OPENGL_CORE, hw_render.version_major = 3, hw_render.version_minor = 3, environment(RETRO_ENVIRONMENT_SET_HW_RENDER, &hw_render)) {
+        LOG_PRINT(RETRO_LOG_INFO, "Using OpenGL 3.3 graphics driver\n");
     } else if (hw_render.context_type = RETRO_HW_CONTEXT_OPENGL_CORE, hw_render.version_major = 3, hw_render.version_minor = 2, environment(RETRO_ENVIRONMENT_SET_HW_RENDER, &hw_render)) {
         LOG_PRINT(RETRO_LOG_INFO, "Using OpenGL 3.2 graphics driver\n");
     } else if (hw_render.context_type = RETRO_HW_CONTEXT_OPENGLES_VERSION, hw_render.version_major = 3, hw_render.version_minor = 2, environment(RETRO_ENVIRONMENT_SET_HW_RENDER, &hw_render)) {
