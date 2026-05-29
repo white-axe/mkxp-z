8f3374f21c17b65d0c5474b04e2126111d2a32aa
刘皓 <whiteaxe@tuta.io>
2026-05-29 16:40:40 -0400

    Add `dirs_exist_ok=True` keyword argument in assets/install-directory.py

--- assets/install-directory.py
+++ assets/install-directory.py
@@ -5,4 +5,4 @@ import sys
 srcdir = sys.argv[1]
 destdir = os.path.join(os.getenv('MESON_INSTALL_DESTDIR_PREFIX'), sys.argv[2])
 
-shutil.copytree(srcdir, destdir)
+shutil.copytree(srcdir, destdir, dirs_exist_ok=True)
