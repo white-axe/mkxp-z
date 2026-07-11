db63995ad3d0aeea4b8c38cfff24642f920efd94
刘皓 <whiteaxe@tuta.io>
2026-07-11 13:12:49 -0400

    Remove unused libraries from subprojects/common/sonametool.py

--- subprojects/common/sonametool.py
+++ subprojects/common/sonametool.py
@@ -12,8 +12,8 @@ host_machine_is_windows = host_machine_system in ('cygwin', 'windows')
 def main() -> str:
     if False:
         pass
-    elif library == 'asound' and host_machine_is_linux:
-        return 'libasound.so.2'
+    elif library == 'decor-0' and host_machine_is_nix:
+        return 'libdecor-0.so.0'
     elif library == 'EGL' and host_machine_is_bsd:
         return "libEGL.so"
     elif library == 'EGL' and host_machine_is_darwin:
@@ -28,22 +28,14 @@ def main() -> str:
         return "libfluidsynth.so.3"
     elif library == 'fluidsynth' and host_machine_is_windows:
         return "fluidsynth.dll"
-    elif library == 'jack' and host_machine_is_nix:
-        return 'libjack.so.0'
-    elif library == 'pipewire-0.3' and host_machine_is_nix:
-        return 'libpipewire-0.3.so.0'
-    elif library == 'pulse-simple' and host_machine_is_nix:
-        return 'libpulse-simple.so.0'
-    elif library == 'usb-1.0' and host_machine_is_nix:
-        return 'libusb-1.0.so.0'
+    elif library == 'udev' and host_machine_is_nix:
+        return 'libudev.so.1'
     elif library == 'wayland-client' and host_machine_is_nix:
         return 'libwayland-client.so.0'
     elif library == 'wayland-cursor' and host_machine_is_nix:
         return 'libwayland-cursor.so.0'
     elif library == 'wayland-egl' and host_machine_is_nix:
         return 'libwayland-egl.so.1'
-    elif library == 'decor-0' and host_machine_is_nix:
-        return 'libdecor-0.so.0'
     elif library == 'X11' and host_machine_is_nix:
         return 'libX11.so.6'
     elif library == 'xcb' and host_machine_is_nix:
@@ -60,8 +52,6 @@ def main() -> str:
         return 'libxkbcommon.so.0'
     elif library == 'Xrandr' and host_machine_is_nix:
         return 'libXrandr.so.2'
-    elif library == 'udev' and host_machine_is_nix:
-        return 'libudev.so.1'
     else:
         raise RuntimeError('Could not determine the soname for the ' + library + ' library on ' + host_machine_system + '. Please add it in subprojects/common/sonametool.py.')
 
