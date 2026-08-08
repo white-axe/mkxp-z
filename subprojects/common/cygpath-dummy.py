d0e00811289f47c428aa6a40f288540bcd2fb5a3
刘皓 <whiteaxe@tuta.io>
2026-08-08 13:06:03 -0400

    Add dummy scripts for windres and cygpath in build configurations where they can't be used

---
+++ subprojects/common/cygpath-dummy.py
@@ -0,0 +1,4 @@
+import sys
+
+print('error: cygpath is not supported when the build machine is not Windows', file=sys.stderr)
+exit(1)
---
+++ subprojects/common/windres-dummy.py
@@ -0,0 +1,4 @@
+import sys
+
+print('error: windres is not supported when the host machine is not Windows', file=sys.stderr)
+exit(1)
