698b8d5f41b659c0e623d5840b67ef2631d80c44
刘皓 <whiteaxe@tuta.io>
2026-08-09 19:13:29 -0400

    Repaint twice in `Graphics::onSizeChanged()` instead of once

--- src/display/graphics.cpp
+++ src/display/graphics.cpp
@@ -1556,9 +1556,10 @@ void Graphics::resizeWindow(int width, int height, bool center) {
 }
 
 void Graphics::onSizeChanged() {
-    // We need to repaint the screen after resizing when using ANGLE's Direct3D 11 backend.
+    // We need to repaint the screen after resizing when using ANGLE's Direct3D 11 backend, twice (probably because of double buffering).
     GFX_LOCK;
     p->checkResize();
+    p->redrawScreen(false);
     p->redrawScreen(false);
     GFX_UNLOCK;
 }
