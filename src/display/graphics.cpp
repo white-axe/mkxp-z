281a25fe01cf7f9cb3566b8b9ff1ddd6aeebd8b5
刘皓 <whiteaxe@tuta.io>
2026-08-10 17:41:06 -0400

    Revert "Don't update frame counters when repainting in `Graphics::onSizeChanged()`"
    This reverts commit 885eb1ca64313878ae38eb0eddfd802ee02ba632.

--- src/display/graphics.cpp
+++ src/display/graphics.cpp
@@ -1054,7 +1054,7 @@ struct GraphicsPrivate {
                               !forceNearestNeighbor && GLMeta::smoothScalingMethod(scaleIsSpecial) == Bilinear);
     }
     
-    void redrawScreen(bool incrementFrameCount = true) {
+    void redrawScreen() {
         screen.composite();
         
         // maybe unspaghetti this later
@@ -1069,15 +1069,8 @@ struct GraphicsPrivate {
             metaBlitBufferFlippedScaled(scRes, scaleIsSpecial, true);
             GLMeta::blitEnd();
             
-            if (incrementFrameCount)
-            {
-                swapGLBuffer();
-                updateAvgFPS();
-            }
-            else
-            {
-                gl.SwapWindow(threadData->window);
-            }
+            swapGLBuffer();
+            updateAvgFPS();
             return;
         }
         
@@ -1127,15 +1120,9 @@ struct GraphicsPrivate {
         
         GLMeta::blitEnd();
         
-        if (incrementFrameCount)
-        {
-            swapGLBuffer();
-            updateAvgFPS();
-        }
-        else
-        {
-            gl.SwapWindow(threadData->window);
-        }
+        swapGLBuffer();
+        
+        updateAvgFPS();
     }
     
     void checkSyncLock() {
@@ -1562,7 +1549,7 @@ void Graphics::onSizeChanged() {
     // We need to repaint the screen after resizing when using ANGLE's Direct3D 11 backend.
     GFX_LOCK;
     p->checkResize();
-    p->redrawScreen(false);
+    p->redrawScreen();
     GFX_UNLOCK;
 }
 
