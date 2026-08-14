41a6795d2381155c3cd337ef070022959ecb4e68
刘皓 <whiteaxe@tuta.io>
2026-08-13 20:26:26 -0400

    Write to git-hash in binary mode in distscript.py
    This is so that the line terminators written to the file are always the
    same regardless of operating system.

--- distscript.py
+++ distscript.py
@@ -10,8 +10,8 @@ meson = shlex.split(os.environ['MESONREWRITE'])[:-1]
 # Write the current Git hash into the release if it isn't already in git-hash
 if len(sys.argv) > 1:
     shutil.copy2(os.path.join(os.environ['MESON_SOURCE_ROOT'], 'git-hash'), os.environ['MESON_DIST_ROOT'])
-    with open(os.path.join(os.environ['MESON_DIST_ROOT'], 'git-hash'), 'a') as f:
-        f.write(subprocess.run(sys.argv[1:], cwd=os.environ['MESON_SOURCE_ROOT'], check=True, stdout=subprocess.PIPE).stdout.decode('UTF-8'))
+    with open(os.path.join(os.environ['MESON_DIST_ROOT'], 'git-hash'), 'ab') as f:
+        f.write(subprocess.run(sys.argv[1:], cwd=os.environ['MESON_SOURCE_ROOT'], check=True, stdout=subprocess.PIPE).stdout)
 
 # Make sure all subprojects are downloaded, even ones that are not used in the current build configuration
 subprocess.run(meson + ['subprojects', 'download'], cwd=os.environ['MESON_SOURCE_ROOT'], check=True, env={**os.environ, 'MESON_PACKAGE_CACHE_DIR': ''})
