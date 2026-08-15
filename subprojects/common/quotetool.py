8bd968557dd27953f3a4ecfacb0bb8846b8e055b
刘皓 <whiteaxe@tuta.io>
2026-08-14 22:27:08 -0400

    Always perform backslash to forward slash conversion when running `quotetool_command`

--- subprojects/common/quotetool.py
+++ subprojects/common/quotetool.py
@@ -1,11 +1,10 @@
 import shlex
 import sys
 
-build_machine_system = sys.argv[1]
-convert_arg0 = sys.argv[2].lower() != 'false'
-argv = sys.argv[3:]
+convert_arg0 = sys.argv[1].lower() != 'false'
+argv = sys.argv[2:]
 
-if convert_arg0 and build_machine_system in ('windows', 'cygwin') and len(argv) > 0:
+if convert_arg0:
     argv[0] = argv[0].replace('\\', '/')
 
 print(' '.join(map(shlex.quote, argv)))
